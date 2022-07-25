70cff8a71151adaa874b106d979e8f4df57b51d5
neveszz <neveszalves@gmail.com>
2022-07-25 16:05:30 -0300

    finished

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, update_task, delete_task
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='list'),
+    path('update/<int:pk>/', update_task, name='update'),
+    path('delete/<int:pk>/', delete_task, name='delete'),
+
 
 ]
