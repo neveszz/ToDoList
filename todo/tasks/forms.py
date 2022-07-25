fdd3408f8589ad50d78c194ec30914f86d6cb821
neveszz <neveszalves@gmail.com>
2022-07-25 16:33:00 -0300

    style

--- todo/tasks/forms.py
+++ todo/tasks/forms.py
@@ -2,6 +2,8 @@ from .models import Task
 from django import forms
 
 class TaskForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Adicionar nova tarefa....'}))
+
     class Meta:
         model = Task
         fields = '__all__'
